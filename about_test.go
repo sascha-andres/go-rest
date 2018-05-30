31c1c30ed7d2af4316175dcef993a54bf91b8e62
Sascha Andres <sascha.andres@outlook.com>
2018-05-30 07:02:12 +0200

    feat: reqrote test

--- about_test.go
+++ about_test.go
@@ -2,17 +2,19 @@ package go_rest
 
 import (
 	"testing"
+
+	"net/http"
+
 	"github.com/matryer/is"
 )
 
 func TestHandleAbout(t *testing.T) {
 	is := is.New(t)
-	srv, err := newServer()
+	_, err := NewServer("localhost", 12345)
 	is.NoErr(err)
-	srv.routes()
-	// req, err := http.NewRequest("GET", "/about", nil)
-	// is.NoErr(err)
-	// w := httptest.NewRecorder()
-	// srv.ServeHTTP(w, r)
-	// is.Equal(w.StatusCode, http.StatusOK)
+	_, err = http.NewRequest("GET", "/about", nil)
+	is.NoErr(err)
+	/*w := httptest.NewRecorder()
+	srv.ServeHTTP(w, req)
+	is.Equal(w.StatusCode, http.StatusOK)*/
 }
