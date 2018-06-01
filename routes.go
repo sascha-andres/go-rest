684134f371a648d1208e345c9413c7b965335c2d
Sascha Andres <sascha.andres@outlook.com>
2018-06-01 19:38:22 +0200

    feat: add templated call

--- routes.go
+++ routes.go
@@ -7,8 +7,12 @@ func (s *Server) routes() {
 	if err != nil {
 		panic(err)
 	}
+	if stat, err := os.Stat("templates"); err == nil && stat.IsDir() {
+		s.router.LoadHTMLGlob("templates/*")
+	}
 	s.router.Handle("GET", "/about/", s.handleAbout())
 	s.router.Handle("GET", "/greet/*to", s.handleGreeting(name))
+	s.router.Handle("GET", "/template/", s.handleTemplate())
 	s.router.Use(s.handleStatic("./static", "/", true))
 }
 
